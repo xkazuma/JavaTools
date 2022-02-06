d78217aeb54c6865fea1a3c2c9c8a9168754e35a
Kazuma Kusu <kkusu@acm.org>
2022-02-06 23:19:50 +0900

    remove a function for measuring RAM consumption.

--- src/main/java/jp/ac/doshisha/mil/x/tools/MeasuringTool.java
+++ src/main/java/jp/ac/doshisha/mil/x/tools/MeasuringTool.java
@@ -6,8 +6,6 @@ import java.util.Map;
 public class MeasuringTool {
     private static Map<String, Long> startTimes  = null;
     private static Map<String, Long> endTimes    = null;
-    private static Map<String, Long> startRAMs   = null;
-    private static Map<String, Long> endRAMs     = null;
 
     public static void measureStart(String key) {
         Long startTime = System.nanoTime();
@@ -15,24 +13,19 @@ public class MeasuringTool {
 
         if(startTimes == null) {
             startTimes = new HashMap<>();
-            startRAMs  = new HashMap<>();
             endTimes = new HashMap<>();
-            endRAMs  = new HashMap<>();
             startTimes.put(key, startTime);
-            startRAMs.put(key,  startRAM);
         } else {
             if (startTimes.containsKey(key)) {
                 System.err.println("Already started measuring.");
                 System.exit(-1);
             }
             startTimes.put(key, startTime);
-            startRAMs.put(key,  startRAM);
         }
     }
 
     public static void measureEnd(String key) {
         Long endTime = System.nanoTime();
-        Long endRAM  = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
 
         if(startTimes == null) {
             System.err.println("Not yet started: " + key);
@@ -40,7 +33,6 @@ public class MeasuringTool {
         } else {
             if (startTimes.containsKey(key)) {
                 endTimes.put(key, endTime);
-                endRAMs.put(key,  endRAM);
             } else {
                 System.err.println("Not yet started: " + key);
                 System.exit(-1);
@@ -63,17 +55,15 @@ public class MeasuringTool {
         }
         Map<String, Long> result = new HashMap<>();
         result.put("Time", endTimes.get(key) - startTimes.get(key) );
-        result.put("RAM",  endRAMs.get(key)  - startRAMs.get(key));
 
         return result;
     }
 
     public static void printResult(String key) {
         var extractingRPathFromHubMeasure = MeasuringTool.getResult(key);
-        System.out.println(
-                String.format(
-                        "RESULTROW-%s\tTime-um:%d RAM-Byte:%d",
-                        key, extractingRPathFromHubMeasure.get("Time"), extractingRPathFromHubMeasure.get("RAM")
-                ));
+        System.out.printf(
+                "RESULTROW-%s\tTime-um:%d",
+                key, extractingRPathFromHubMeasure.get("Time")
+        );
     }
 }
