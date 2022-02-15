9feb24385842937f1fd571bf16ad4ed025e1de69
Kazuma Kusu <kkusu@acm.org>
2022-02-16 02:17:23 +0900

    Create ObjectSizeFetcher

---
+++ src/main/java/jp/ac/doshisha/mil/x/tools/ObjectSizeFetcher.java
@@ -0,0 +1,15 @@
+package jp.ac.doshisha.mil.x.tools;
+
+import java.lang.instrument.Instrumentation;
+
+public class ObjectSizeFetcher {
+    private static Instrumentation instrumentation;
+
+    public static void premain(String args, Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    public static long getObjectSize(Object o) {
+        return instrumentation.getObjectSize(o);
+    }
+}
